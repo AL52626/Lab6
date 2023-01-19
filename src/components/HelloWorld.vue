4a2993f990dab417418855ba62bda01d5e9aef14
Andrej L <al52626@fer.hr>
2023-01-20 00:56:03 +0100

    https://github.com/AL52626/Lab6.git

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="text-2xl	m-2 font-bold">
+    <h1>{{ welcomeText }}</h1>
+  </div>
+  <v-container>
+    <v-col class="d-flex" cols="1" sm="3">
+      <v-select :items="options" label="Outlined style" v-model="selectedOption" outlined></v-select>
+    </v-col>
+
+  </v-container>
+  <div class="p-3">
+    <p class="text-lg font-semibold">Selected option is {{ selectedOption }}</p>
+    <v-btn v-if="selectedOption" depressed elevation="2" @click="onClick">Show me a player</v-btn>
+  </div>
+
+  <div v-if="showPicture" class="p-3">
+    <v-img v-if="this.selectedOption == 'Seria A'" max-height="300" max-width="450"
+      src="https://www.thesun.co.uk/wp-content/uploads/2020/05/NINTCHDBPICT000001387914.jpg"></v-img>
+    <v-img v-if="this.selectedOption == 'Premiere Leauge'" max-height="300" max-width="450"
+      src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/10/26/17/drogba-3.jpg?quality=75&width=982&height=726&auto=webp"></v-img>
+    <v-img v-if="this.selectedOption == 'La Liga'" max-height="300" max-width="450"
+      src="https://images.beinsports.com/jq87X4eACgg7iqh4GJMK_wBs3GQ=/full-fit-in/1000x0/ronaldinho-cropped_1nsieiiy254011d58qieaiq0f5.jpg"></v-img>
+
+    <p class="font-semibold italic font-mono text-lg margin">{{ playerName }}</p>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data: () => ({
+    welcomeText: 'Hi, choose a football leauge you like',
+    selectedOption: '',
+    player: '',
+    showPicture: false,
+    options: [
+      'Seria A',
+      'Premiere Leauge',
+      'La Liga',
+    ]
+  }),
+  methods: {
+    onClick() {
+      this.showPicture = true;
+    }
+  },
+  computed: {
+    playerName() {
+      if (this.selectedOption == 'Seria A')
+        return 'Zidane'
+      if (this.selectedOption == 'Premiere Leauge')
+        return 'Drogba'
+      if (this.selectedOption == 'La Liga')
+        return 'Ronaldinho'
+      else return ''
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.margin {
+  margin: 10px;
 }
 </style>
