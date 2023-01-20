3be3d9fe7757dc417e4b64bc4d50fbade008b5b2
Andrej L <al52626@fer.hr>
2023-01-20 01:33:55 +0100

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/',
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
